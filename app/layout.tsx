c7ce92ad872e544887d539a54eec549f644cc257
Muhammad Yaseen <yaseenonlinepk@gmail.com>
2026-09-21 09:37:25 +0000

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -68,7 +68,7 @@ export default function RootLayout({
   children: ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>{children}</body>
     </html>
   );
